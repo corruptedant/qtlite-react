1655b2837c64061156f033b20838ee1a09fc7e9b
CorruptedAnt <corruptedant@users.noreply.github.com>
2022-03-07 11:20:10 +0800

    backend: split settings into development and production

---
+++ backend/qtlite/settings/production.py
@@ -0,0 +1,48 @@
+import dj_database_url
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['corruptedant-quicktrack.herokuapp.com',
+                 'next-quicktrack.herokuapp.com', 'v3-quicktrack.herokuapp.com']
+
+# SSL / CSRF / CSP
+
+SECURE_SSL_REDIRECT = True
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+    }
+}
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
