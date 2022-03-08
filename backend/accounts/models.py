f933bab783a81f907fd4873cc09326d0065ff0a5
CorruptedAnt <corruptedant@users.noreply.github.com>
2022-03-08 18:12:13 +0800

    backend: added description and UUID for Debit and Credit
    The objects are concatenated in the front end, which results in key clashing

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.conf import settings
 from django.db import models
 
@@ -11,7 +13,9 @@ class Account(models.Model):
         return self.name
 
 class Debit(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     date = models.DateField()
+    description = models.CharField(max_length=128, null=False, blank=False)
     value = models.DecimalField(max_digits=14, decimal_places=4)
     account = models.ForeignKey(Account, related_name='debits', on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
@@ -20,7 +24,9 @@ class Debit(models.Model):
         return f'{self.account} / {self.value}'
 
 class Credit(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     date = models.DateField()
+    description = models.CharField(max_length=128, null=False, blank=False)
     value = models.DecimalField(max_digits=14, decimal_places=4)
     account = models.ForeignKey(Account, related_name='credits', on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
