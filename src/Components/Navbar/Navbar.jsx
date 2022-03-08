f81cd7b9b3bebddc4887da53f3663cd630702b0c
CorruptedAnt <corruptedant@users.noreply.github.com>
2022-03-08 17:28:26 +0800

    frontend: refactor Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -11,23 +11,24 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
     return (
-        <nav className="flex items-center px-2 shadow sticky top-0 z-50 bg-white">
-            <div className="flex items-center flex-shrink-0 p-2">
+        <nav className="flex px-2 justify-between shadow sticky top-0 z-50 bg-white">
+            <div className="inline-flex items-center flex-shrink-0 p-2">
                 <img src={quicktrack} alt="" className="h-14 w-14" />
-                <h1 className="text-2xl pl-4 hidden xl:block">
-                    Quicktrack Lite
-                </h1>
+                {/* FIXME: Correctly centers the middle portion. */}
+                <h1 className="text-2xl pl-4 hidden">Quicktrack Lite</h1>
             </div>
-            <div className="flex items-center mx-auto">
-                <Link to="/">
-                    <NavIcon Icon={HomeIcon} title={'Home'} />
-                </Link>
-                <Link to="/accounts">
-                    <NavIcon Icon={UsersIcon} title={'Accounts'} />
-                </Link>
-                <Link to="/info">
-                    <NavIcon Icon={InformationCircleIcon} title={'Info'} />
-                </Link>
+            <div className="flex justify-center items-center">
+                <div className="flex items-center ">
+                    <Link to="/">
+                        <NavIcon Icon={HomeIcon} title={'Home'} />
+                    </Link>
+                    <Link to="/accounts">
+                        <NavIcon Icon={UsersIcon} title={'Accounts'} />
+                    </Link>
+                    <Link to="/info">
+                        <NavIcon Icon={InformationCircleIcon} title={'Info'} />
+                    </Link>
+                </div>
             </div>
             <div className="flex items-center">
                 <div className="flex items-center cursor-pointer">
