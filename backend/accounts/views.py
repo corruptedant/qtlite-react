c79814321a57d6c52f88df9fcb2c1785b4deb3f7
CorruptedAnt <corruptedant@users.noreply.github.com>
2022-03-08 18:13:28 +0800

    backend: fix 500 when not logged in for accounts endpoint

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 from rest_framework import generics
 
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsOwnerOrReadOnly
+
 from .serializers import AccountSerializer
 
 from .models import Account
@@ -9,6 +12,7 @@ from .models import Account
 # Create your views here.
 class AccountsList(generics.ListCreateAPIView):
     serializer_class = AccountSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
     def get_queryset(self):
         return Account.objects.filter(user=self.request.user)
