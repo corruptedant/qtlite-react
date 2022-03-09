00651190b00de0050f66485617b1364baa54bb64
CorruptedAnt <corruptedant@users.noreply.github.com>
2022-03-09 22:35:52 +0800

    build(frontend): :package: Added eslint, eslint-plugin-react, eslint-plugin-react-hooks and vite-plugin-eslint
    To provide better intellisense and improve code quality

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslintPlugin from '@nabla/vite-plugin-eslint'
+
+const eslintOptions = {
+    useEslintrc: true,
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), eslintPlugin(eslintOptions)],
     build: {
         assetsDir: 'static',
     },
