f8ece2ab2e11f7aa8a83d1b0c635e8aeac089a2b
CorruptedAnt <corruptedant@users.noreply.github.com>
2022-03-11 14:23:03 +0800

    fix(backend): :bug: CreditSerializer and DebitSerializer now only works with current user's Accounts
    It was able to fetch all Accounts before.

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,13 +1,50 @@
+from urllib import request
 from rest_framework import serializers
 
 from .models import (Account, Debit, Credit)
 
 class CreditSerializer(serializers.ModelSerializer):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        user_accounts = Account.objects.filter(user=self._user)
+        self.fields['account'] = serializers.PrimaryKeyRelatedField(
+            required=False,
+            many=False,
+            read_only=False,
+            queryset=user_accounts,
+            default=user_accounts
+        )
+
+    @property
+    def _user(self):
+        request = self.context.get('request', None)
+        if request:
+            return request.user
+
     class Meta:
         model = Credit
         fields = ['id', 'date', 'description', 'credit', 'date_created']
 
 class DebitSerializer(serializers.ModelSerializer):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        user_accounts = Account.objects.filter(user=self._user)
+        self.fields['account'] = serializers.PrimaryKeyRelatedField(
+            required=False,
+            many=False,
+            read_only=False,
+            queryset=user_accounts,
+            default=user_accounts
+        )
+
+    @property
+    def _user(self):
+        request = self.context.get('request', None)
+        if request:
+            return request.user
+
     class Meta:
         model = Debit
         fields = ['id', 'date', 'description', 'debit', 'date_created']
